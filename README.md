404a26bf94bef7fca7f98b52b79c0d92f9c966a7
Manuel Spagnolo <manuel.spagnolo@proton.me>
2026-03-10 09:02:50 +0100

    test: check configuration files in tests

--- README.md
+++ README.md
@@ -22,13 +22,13 @@ Optionally, programs can be accompained by a man page, whose naming convention f
 Executing the following command will install latest version of `mybin` (for example, downloading `mybin-linux-amd64`)
 
 ```sh
-curl -s https://shikaan.github.io/sup/install | REPO=myuser/mybin sh -
+curl -s https://shikaan.github.io/sup/install | REPO=owner/repo sh -
 ```
 
 The name of the binary is inferred from the `REPO` variable. You can override it with `BIN`
 
 ```sh
-curl -s https://shikaan.github.io/sup/install | REPO=myuser/myrepo BIN=mybin sh -
+curl -s https://shikaan.github.io/sup/install | REPO=owner/repo BIN=mybin sh -
 ```
 
 ### Uninstall
@@ -36,13 +36,13 @@ curl -s https://shikaan.github.io/sup/install | REPO=myuser/myrepo BIN=mybin sh 
 Same as above, but it will uninstall `mybin`
 
 ```sh
-curl -s https://shikaan.github.io/sup/uninstall | REPO=myuser/mybin sh -
+curl -s https://shikaan.github.io/sup/uninstall | REPO=owner/repo sh -
 ```
 
 Or, again, with the `BIN` override
 
 ```sh
-curl -s https://shikaan.github.io/sup/uninstall | REPO=myuser/myrepo BIN=mybin sh -
+curl -s https://shikaan.github.io/sup/uninstall | REPO=owner/repo BIN=mybin sh -
 ```
 
 ## 📝 Example
--- tests.sh
+++ tests.sh
@@ -10,7 +10,7 @@ SUDO=
 [ "$(id -u)" -ne 0 ] && SUDO="sudo -E"
 
 spec() {
-  local fn="$1"
+  fn="$1"
 
   printf '%s\n' "$fn..."
   total=$((total + 1))
@@ -25,58 +25,79 @@ spec() {
   fi
 }
 
+config_init() {
+  file="$HOME/$1"
+  rm -f "$file"
+  touch "$file"
+}
+
+has_valid_config() {
+  file="$HOME/$1"
+  manpage="${2:-}"
+
+  grep -Fq "$HOME/.local/bin" "$file" || return 1
+
+  if [ -n "$manpage" ]; then
+    grep -Fq "$HOME/.local/share/man" "$file" || return 1
+  fi
+}
+
 test_backward_compatibility_nochecksum_install() {
-  cat ./install | REPO=shikaan/shmux sh -
-  [ -f "$HOME/.local/bin/shmux" ]
+  config_init ".bashrc"
+  REPO=shikaan/shmux sh - < ./install
+  [ -f "$HOME/.local/bin/shmux" ] && has_valid_config ".bashrc"
 }
 
 test_backward_compatibility_nochecksum_warning() {
-  cat ./install | REPO=shikaan/shmux sh - > $output
-  [ $(grep -c "WARNING" $output) -eq "1" ]
+  REPO=shikaan/shmux sh - < ./install > $output
+  [ "$(grep -c "WARNING" $output)" -eq "1" ]
 }
 
 test_backward_compatibility_nochecksum_uninstall() {
-  cat ./uninstall | REPO=shikaan/shmux sh -
+  REPO=shikaan/shmux sh - < ./uninstall
   [ ! -f "$HOME/.local/bin/shmux" ]
 }
 
 test_backward_compatibility_withsudo_install() {
-  $SUDO sh -c "cat ./install | REPO=shikaan/shmux sh -"
-  [ -f "$HOME/.local/bin/shmux" ]
+  config_init ".bashrc"
+  $SUDO sh -c "REPO=shikaan/shmux sh - < ./install"
+  [ -f "$HOME/.local/bin/shmux" ] && has_valid_config ".bashrc"
 }
 
 test_backward_compatibility_withsudo_uninstall() {
   # simulate package present in old location
-  $SUDO cp "$HOME/.local/bin/shmux" "/usr/local/bin/shmux" 
-  $SUDO sh -c "cat ./uninstall | REPO=shikaan/shmux sh -"
-  [ ! -f "$HOME/.local/bin/shmux" ]
-  [ ! -f "/usr/local/bin/shmux" ]
+  $SUDO cp "$HOME/.local/bin/shmux" "/usr/local/bin/shmux"
+  $SUDO sh -c "REPO=shikaan/shmux sh - < ./uninstall"
+  [ ! -f "$HOME/.local/bin/shmux" ] && [ ! -f "/usr/local/bin/shmux" ]
 }
 
 test_withchecksum_nomanpage() {
-  cat ./install | REPO=shikaan/keydex sh -
-  [ -f "$HOME/.local/bin/keydex" ]
+  config_init ".bashrc"
+  REPO=shikaan/keydex sh - < ./install
+  [ -f "$HOME/.local/bin/keydex" ]  && has_valid_config ".bashrc"
 }
 
 test_withchecksum_nomanpage_nowarning() {
-  cat ./install | REPO=shikaan/keydex sh -
-  cat ./install | REPO=shikaan/keydex sh - > $output
-  [ $(grep -c "WARNING" $output) -eq "0" ]
+  REPO=shikaan/keydex sh - < ./install > $output
+  [ "$(grep -c "WARNING" $output)" -eq "0" ]
 }
 
 test_withchecksum_nomanpage_uninstall() {
-  cat ./uninstall | REPO=shikaan/keydex sh -
-  [ ! -f "$HOME/.local/bin/shmux" ]
+  REPO=shikaan/keydex sh - < ./uninstall
+  [ ! -f "$HOME/.local/bin/keydex" ]
 }
 
 test_withchecksum_manpage() {
-  cat ./install | REPO=shikaan/lifp sh -
-  [ -f "$HOME/.local/bin/lifp" ] && [ -f "$HOME/.local/share/man/man1/lifp.1" ]
+  config_init ".bashrc"
+  REPO=shikaan/lifp sh - < ./install
+  [ -f "$HOME/.local/bin/lifp" ] && \
+    [ -f "$HOME/.local/share/man/man1/lifp.1" ] && \
+    has_valid_config ".bashrc" "1"
 }
 
 test_withchecksum_manpage_uninstall() {
-  cat ./uninstall | REPO=shikaan/lifp sh -
-  [ ! -f "$HOME/.local/bin/shmux" ] && [ ! -f "$HOME/.local/share/man/man1/lifp.1" ]
+  REPO=shikaan/lifp sh - < ./uninstall
+  [ ! -f "$HOME/.local/bin/lifp" ] && [ ! -f "$HOME/.local/share/man/man1/lifp.1" ]
 }
 
 specs=$(sed -n 's/^[[:space:]]*\(test_[A-Za-z0-9_]*\)[[:space:]]*()[[:space:]]*{[[:space:]]*$/\1/p' "$0")
